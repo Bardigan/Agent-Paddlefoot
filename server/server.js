3a614f1937b2b91945469f488038aa1f39b14f5b
Sergey Golubev <golubev.irk@gmail.com>
2025-02-05 17:05:37 +0100

    initial server

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import records from "./routes/record.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/record", records);
+
+// handle default /
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
