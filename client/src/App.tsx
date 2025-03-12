80ccdb60be477d0a114c15a71af7d835a3a0af22
Sergey Golubev <golubev.irk@gmail.com>
2025-03-12 15:51:08 +0100

    re2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,20 @@ import Navbar from "./components/Navbar";
 import { GameContextProvider } from './context/GameContext';
 import { useContext } from "react";
 import { GameContext } from './context/GameContext';
+import { useEffect } from 'react';
 import './App.scss';
 
 const AppWrapper = () => {
   const context = useContext(GameContext);
+  
+  useEffect(() => {
+      const storedToken = localStorage.getItem('token');
+      const tokenTimestamp = localStorage.getItem('tokenTimestamp');
+      if (storedToken && tokenTimestamp) {
+        context?.setToken(storedToken);
+      }
+    }, []);
+
   return (
     <>
       {context?.token && <Navbar />}
--- client/src/context/GameContext.tsx
+++ client/src/context/GameContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, ReactNode, useEffect } from 'react';
+import React, { createContext, useState, ReactNode } from 'react';
 
 interface GameContextType {
   score: number;
@@ -19,23 +19,6 @@ export const GameContextProvider: React.FC<DummyProviderProps> = ({ children }) 
   const [score, setScore] = useState<number>(0);
   const [gameStatus, setGameStatus] = useState<boolean | null>(null);
   const [token, setToken] = useState<string | null>(null);
-
-  useEffect(() => {
-    const storedToken = localStorage.getItem('token');
-    const tokenTimestamp = localStorage.getItem('tokenTimestamp');
-    
-    if (storedToken && tokenTimestamp) {
-      const tokenAge = Date.now() - parseInt(tokenTimestamp, 10);
-      const eightHoursInMilliseconds = 8 * 60 * 60 * 1000;
-
-      if (tokenAge < eightHoursInMilliseconds) {
-        setToken(storedToken);
-      } else {
-        //localStorage.removeItem('token');
-        //localStorage.removeItem('tokenTimestamp');
-      }
-    }
-  }, []);
 
   const customValues = {
     score,
