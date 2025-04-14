ddd971d470a2c838d2a113b555adb68d7c9b3a93
Sergey Golubev <golubev.irk@gmail.com>
2025-04-14 20:16:43 +0200

    cleanup

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ import records from "./routes/record.js";
 import dotenv from "dotenv";
 import jwt from 'jsonwebtoken';
 import db from "./db/connection.js";
-import bcrypt from "bcrypt"; // For password hashing
-import { body, validationResult } from "express-validator"; // For input validation
+import bcrypt from "bcrypt";
+import { body, validationResult } from "express-validator";
 
 dotenv.config();
 
@@ -15,7 +15,7 @@ const app = express();
 
 const generateToken = (user) => {
   const payload = { userId: user._id, username: user.username };
-  const options = { expiresIn: '8h' }; // Token expiration time
+  const options = { expiresIn: '8h' };
   return jwt.sign(payload, secretKey, options);
 };
 
@@ -23,10 +23,10 @@ const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
-  if (!token) return res.sendStatus(401); // Unauthorized
+  if (!token) return res.sendStatus(401);
 
   jwt.verify(token, secretKey, (err, user) => {
-    if (err) return res.sendStatus(403); // Forbidden
+    if (err) return res.sendStatus(403);
 
     req.user = user;
     next();
@@ -34,9 +34,9 @@ const authenticateToken = (req, res, next) => {
 };
 
 app.use(cors({
-  origin: '*', // Allow all origins
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(express.json());
 app.use("/score", authenticateToken, records);
@@ -45,7 +45,6 @@ app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-// Login route
 app.post(
   "/login",
   [
@@ -63,7 +62,6 @@ app.post(
     try {
       const usersCollection = await db.collection("users");
 
-      // Find user in the database
       const user = await usersCollection.findOne({ username });
 
       if (user && bcrypt.compareSync(password, user.password)) {
@@ -96,36 +94,30 @@ app.post(
     try {
       const usersCollection = await db.collection("users");
 
-      // Check if the username already exists
       const existingUser = await usersCollection.findOne({ username });
 
       if (existingUser) {
-        return res.status(409).json({ message: "Username already exists" }); // Conflict
+        return res.status(409).json({ message: "Username already exists" });
       }
 
-      // Hash the password
       const hashedPassword = bcrypt.hashSync(password, 10);
 
-      // Create a new user
       const newUser = {
         username,
         password: hashedPassword,
       };
 
-      // Add the new user to the database
       const result = await usersCollection.insertOne(newUser);
 
-      // Generate a token for the new user
       const token = generateToken({ _id: result.insertedId, username: newUser.username });
 
-      res.status(201).json({ token }); // Created
+      res.status(201).json({ token });
     } finally {
-      //await client.close();
+      // No need to close the connection here, as it's handled by the MongoDB driver
     }
   }
 );
 
-// start the Express server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
